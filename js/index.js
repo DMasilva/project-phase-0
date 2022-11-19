9c4486d5efa77cf79443d8da3586fee1f2f7952b
David <david.owuor@student.moringaschool.com>
2022-11-19 14:18:45 +0300

    edited html, css files

---
+++ js/index.js
@@ -0,0 +1,54 @@
+//expressions; => contain data and operators na inareturn value
+//constant expresion;
+
+// let name = "jack"
+
+//boolean expression
+// truthy && falsey
+// Boolean()
+
+// let day = "Monday"
+// let assignment;
+
+// //logic programming
+
+// if(day)
+
+// const name = "Masilva";
+// const friend = "ben";
+// const cook = false;
+
+//logic
+//masilva akipika, ben ataosha vyombo, else ben atalala
+
+//statement ? true : false;
+
+//const result = cook ? "ben wash utensil" : "ben sleeps";
+//console.log(result);
+
+// const sentence = `${friend} has decided to ` + (cook ? "wash utensils" : "sleep") + ` beacause of ${name}`;
+// console.log(sentence);
+ 
+//results = `${siste} has decided to ` + (cook ? "wash utensils" : "sleep") + ` because of ${name}`;
+
+//console.log(results);
+
+//working with strings 
+// defined using three symbols
+// 1. ("string"), ('string'), (`string`)
+//backtick is used during interpolation
+//what is interpolation? - this is adding values to a string
+// how interpolation works
+
+// const name = "tracey";
+// const secondName = "claudine";
+// const number = 20;
+
+// const sentence = `${name} and ${secondName} have ${number}`;
+// console.log(sentence);
+
+
+//lookup expression
+
+
+

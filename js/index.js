aaf2a1a4f557ba9c5a19cc8706dda66e53c45a39
David <david.owuor@student.moringaschool.com>
2022-11-21 03:40:41 +0300

    initializing updates in style.css

--- js/index.js
+++ js/index.js
@@ -51,10 +51,25 @@
 //lookup expression
 
 function myFunction() {
-    const - document.getElementById("dot");
-    const moreText - documentgetElementById("continue")
+    const dot = document.getElementById("dots");
+    const moreText = document.getElementById("more");
+    const btnText = document.getElementById("myBtn");
+
+    if(dot.style.display === "none"){
+        dot.style.display - "inline";
+        btnText.innerHTML - "Read more";
+        moreText.style.display - "none";
+
+
+    }else{
+        dot.style.display = "none"; 
+        btnText.innerHTML = "Read less";
+        moreText.style.display - "inline";
+
+    }
 
 }
+console.log(dots, moreText, btnText);
 
 
 

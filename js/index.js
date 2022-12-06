7ecce2711b20343669c2d9891edb76b1cc0f218b
David <david.owuor@student.moringaschool.com>
2022-12-06 16:49:07 +0300

    Updated portfolio

--- js/index.js
+++ js/index.js
@@ -1,25 +1,11 @@
 
+document.addEventListener('DOMContentLoaded',()=>{
 
-// function myFunction() {
-//     const dot = document.getElementById("dots");
-//     const moreText = document.getElementById("more");
-//     const btnText = document.getElementById("myBtn");
-
-//     if(dot.style.display === "none"){
-//         dot.style.display - "inline";
-//         btnText.innerHTML - "Read more";
-//         moreText.style.display - "none";
-
-
-//     }else{
-//         dot.style.display = "none"; 
-//         btnText.innerHTML = "Read less";
-//         moreText.style.display - "inline";
-
-//     }
-
-// }
-// console.log(dots, moreText, btnText);
+    const button = document.querySelector('.submit-button')
+    button.addEventListener('click',(e)=>{
+        e.preventDefault()
+    })
+})
 
 
 
